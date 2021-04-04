17a563479874c33e5f815adcd6d3bb5fd0b90771
d1mentor <gerasimenkot92@gmail.com>
2021-04-04 15:32:48 +0200

    Unicode suit symbols

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
-numbers = %w[1 2 3 4 5 6 7 8 9 10 J Q K A]
-color = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-puts "#{numbers.sample} #{color.sample}"
+# Этот код необходим только при использовании unicode-символов на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# Создадим массив с набором мастей, вместо названия масти — иконка-символ
+suits = ["♦", "♠", "♥ ", "♣"]
+
+puts "#{values.sample}#{suits.sample}"
+
